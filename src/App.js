314a3f70b73692a0f8f0cdba08bc77772ed500ed
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-15 20:15:09 +0530

    feat: React navbar menu component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Topbar from "./components/topbar/Topbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Topbar.css'
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import LanguageIcon from '@mui/icons-material/Language';
+import SettingsIcon from '@mui/icons-material/Settings';
+// import { NotificationsIcon, LanguageIcon, SettingsIcon} from '@mui/icons-material';
+
+const Topbar = () => {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+            <div className="topLeft">
+                <div className="logo">
+                    devsAdmin
+                </div>
+            </div>
+            <div className="topRight">
+                <div className="topbarIconContainer">
+                    <NotificationsIcon/>
+                    <span className="topIconBadge">2</span>
+                </div>
+                <div className="topbarIconContainer">
+                    <LanguageIcon/>
+                    <span className="topIconBadge">2</span>
+                </div>
+                <div className="topbarIconContainer">
+                    <SettingsIcon/>
+                </div>
+                <div className="topbarIconContainer">
+                    <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+                </div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topbar
