bad3a442456ca3aa16b02a93f194cffe2958ab20
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-16 21:59:54 +0530

    feat: Created info stats charts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./Home.css";
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+import Chart from '../../components/chart/Chart';
+import { userData } from '../../dummyData';
 
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo/>
+            <Chart data = {userData} title = "User Analytics" dataKey = "Active User" grid/>
         </div>
     )
 }
