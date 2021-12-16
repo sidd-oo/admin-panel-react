d7fc36516d38066d5365daa60e7466287412fb12
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-16 21:02:59 +0530

    Created Admin dashboard info widgets under Home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Home.css";
+import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <FeaturedInfo/>
+        </div>
+    )
+}
+
+export default Home
+   
