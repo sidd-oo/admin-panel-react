537a20f572dacd71489f9b8e1b4b33f0e986f213
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-19 21:11:44 +0530

    refactor: Added routes for the users and product page

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,6 +13,7 @@ import AttachMoney from '@mui/icons-material/AttachMoney';
 import Report from '@mui/icons-material/Report';
 import MailOutline from '@mui/icons-material/MailOutline';
 import Timeline from '@mui/icons-material/Timeline';
+import { Link } from 'react-router-dom';
 
 
 const Sidebar = () => {
@@ -22,10 +23,12 @@ const Sidebar = () => {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem active">
-                            <LineStyleIcon className="sidebarIcon"/>
-                            Home
-                        </li>
+                        <Link to = "/" className='link'>
+                            <li className="sidebarListItem active">
+                                <LineStyleIcon className="sidebarIcon"/>
+                                Home
+                            </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <TimelineIcon className="sidebarIcon"/>
                             Analytics
@@ -39,14 +42,18 @@ const Sidebar = () => {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Quick Menu</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem ">
-                            <PermIdentityIcon className="sidebarIcon"/>
-                            Users
-                        </li>
-                        <li className="sidebarListItem">
-                            <StorefrontIcon className="sidebarIcon"/>
-                            Products
-                        </li>
+                        <Link to = "/users" className="link">
+                            <li className="sidebarListItem ">
+                                <PermIdentityIcon className="sidebarIcon"/>
+                                Users
+                            </li>
+                        </Link>
+                        <Link to = "/products" className="link">
+                            <li className="sidebarListItem">
+                                <StorefrontIcon className="sidebarIcon"/>
+                                Products
+                            </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <AttachMoney className="sidebarIcon"/>
                             Transactions
